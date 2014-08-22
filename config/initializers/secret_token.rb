953d8afd9db3ac4aa1a87868993cccd0567ea9f9
Tom Brennan <tommybrennan@gmail.com>
2014-08-22 13:55:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItemTrackApp::Application.config.secret_key_base = '5aab24db5e3ac09b9a61cb3411f62afd70b85a39061360e22ad6352adafb8c8730e078e124fd89257d184ca772443a3c95001f67fb55e294f3ca668b09c44654'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItemTrackApp::Application.config.session_store :cookie_store, key: '_item_track_app_session'
